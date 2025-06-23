ce87672adcddde6624e0435c94761ae91180c968
JDobrzanski <jakubdobrzanski@icloud.com>
2025-06-24 01:53:06 +0200

    changes

--- utils.py
+++ utils.py
@@ -1,9 +1,21 @@
-# przykladowe funkcje utils .py
+"""Funkcje matematyczne"""
+
+
 def add(a: int, b: int) -> int:
+    """suma 2 liczb"""
     return a + b
+
+
 def subtract(a: int, b: int) -> int:
+    """roznica 2 liczb"""
     return a - b
+
+
 def multiply(a: int, b: int) -> int:
+    """iloczyn 2 liczb"""
     return a * b
-def divide(a: int, b: int) -> float :
+
+
+def divide(a: int, b: int) -> float:
+    """iloraz 2 liczb"""
     return a / b
